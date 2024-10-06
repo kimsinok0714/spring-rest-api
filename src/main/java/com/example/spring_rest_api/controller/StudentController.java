a2a489c27b956f5af001b2c76f91ef24e6c62439
kimsinok0714 <javaenjoy0714@gmail.com>
2024-10-06 15:53:48 +0900

    Update

--- src/main/java/com/example/spring_rest_api/controller/StudentController.java
+++ src/main/java/com/example/spring_rest_api/controller/StudentController.java
@@ -1,15 +1,18 @@
 package com.example.spring_rest_api.controller;
 
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.spring_rest_api.entity.Student;
 import com.example.spring_rest_api.service.StudentService;
 import lombok.RequiredArgsConstructor;
 import java.util.List;
-
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 
@@ -28,8 +31,61 @@ public class StudentController {
      */
     @GetMapping("/students")
     public ResponseEntity<List<Student>> getStudents() {
-        return ResponseEntity.ok(studentService.getStudentAll());
+        //return ResponseEntity.ok(studentService.getStudentAll());
+        return new ResponseEntity<>(studentService.getStudentAll(), HttpStatus.OK);
     }
+
+    /*
+     * 학생 정보 등록
+     * POST /students
+     * 201 CREATE
+     */
+    @PostMapping("/students")
+    public ResponseEntity<Student> createStuent(@RequestBody Student student) {
+        Student savedStudent = studentService.addStudent(student);
+        return new ResponseEntity<>(savedStudent, HttpStatus.CREATED);
+    }
+
+
+    /*
+     * 학생 상세 조회
+     * GET /students/{id}
+     * 200 OK
+     */
+    @GetMapping("/students/{id}")
+    public ResponseEntity<Student> getStudentById(@PathVariable(value = "id") Long id) {
+        Student student = studentService.getStudentByStudntId(id);
+        if (student == null) {
+            throw new RuntimeException("해당 학생이 존재하지 않습니다.");
+        }
+        return new ResponseEntity<>(student, HttpStatus.OK);
+    }
+    
+   
+
+    /*
+     * 학생 정보 수정
+     * POST /students/{id}
+     * 204 No Content
+     */
+    @PutMapping("/students/{id}")
+    public ResponseEntity<Student> updateStudent(@PathVariable(value = "id") Long id, @RequestBody Student student) {
+        student.setStudentId(id);
+        Student updatedStudent = studentService.updateStudent(student);
+        return new ResponseEntity<>(updatedStudent, HttpStatus.NO_CONTENT);
+    }
+
+    /*
+     * 학생 정보 삭제
+     * POST /students/{id}
+     * 204 No Content
+     */
+    @PostMapping("/students/{id}")
+    public ResponseEntity<Void> postMethodName(@PathVariable(value = "id") Long id) {
+        studentService.deleteStudent(id);   
+        return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+    }
+    
 
 }
 
